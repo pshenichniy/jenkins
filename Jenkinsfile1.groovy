0ceeff2e0b050dec76d0da301932cf740d36a39f
Пшеничный Илья Викторович <pshenichnyj@secretgroup.ru>
2022-01-22 23:16:48 +0300

    comment

--- Jenkinsfile1.groovy
+++ Jenkinsfile1.groovy
@@ -1,40 +1,40 @@
-pipeline {
-    agent any
-    parameters {
-        string(name: 'Greeting', defaultValue: 'Hello', description: 'How should I greet the world?')
-    }
-    stages {
-        stage('Example') {
-            steps {
-                echo "${params.Greeting} World!"
-            }
-        }
-        stage('Install OpenVino') {
-            steps {
-                echo 'Install open Vino'
-            }
-        }
-        stage('Сборка') {
-            steps {
-                echo 'Выполняем команды для сборки'
-            }
-        }
-
-        stage('Тестирование') {
-            steps {
-                echo 'Тестируем нашу сборку'
-            }
-        }
-        stage('Развертывание') {
-            steps {
-                echo 'Переносим код в рабочую среду или создаем артефакт'
-
-
-            }
-        }
-
-    }
-}
+//pipeline {
+//    agent any
+//    parameters {
+//        string(name: 'Greeting', defaultValue: 'Hello', description: 'How should I greet the world?')
+//    }
+//    stages {
+//        stage('Example') {
+//            steps {
+//                echo "${params.Greeting} World!"
+//            }
+//        }
+//        stage('Install OpenVino') {
+//            steps {
+//                echo 'Install open Vino'
+//            }
+//        }
+//        stage('Сборка') {
+//            steps {
+//                echo 'Выполняем команды для сборки'
+//            }
+//        }
+//
+//        stage('Тестирование') {
+//            steps {
+//                echo 'Тестируем нашу сборку'
+//            }
+//        }
+//        stage('Развертывание') {
+//            steps {
+//                echo 'Переносим код в рабочую среду или создаем артефакт'
+//
+//
+//            }
+//        }
+//
+//    }
+//}
 class Windows {
     static final Map SUPPORTED_DEVICES = [
             'ADL': [deviceType: 'PUX',label: 'adl']
